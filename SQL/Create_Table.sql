e95e10baaf2a82a8c4fedd65098f8bc703613389
Afif <afifjunihar@gmail.com>
2024-09-28 02:22:07 +0700

    SQL Table

--- SQL/Create_Table.sql
+++ SQL/Create_Table.sql
@@ -1,29 +1,73 @@
-CREATE TABLE ms_storage_location (
-    location_id VARCHAR(10) PRIMARY KEY,
-    location_name VARCHAR(100)
-);
+/****** Object:  Table [dbo].[ms_storage_location]    Script Date: 28/09/2024 02.19.14 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ms_storage_location]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [dbo].[ms_storage_location](
+	[location_id] [varchar](10) NOT NULL,
+	[location_name] [varchar](100) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[location_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
 
-CREATE TABLE ms_user (
-    user_id BIGINT PRIMARY KEY,
-    user_name VARCHAR(20),
-    password VARCHAR(50),
-    is_active BIT
-);
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[ms_user]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [dbo].[ms_user](
+	[user_id] [bigint] IDENTITY(1,1) NOT NULL,
+	[user_name] [varchar](20) NULL,
+	[password] [varchar](50) NULL,
+	[is_active] [bit] NULL,
+ CONSTRAINT [PK__ms_user__B9BE370FB35194F7] PRIMARY KEY CLUSTERED 
+(
+	[user_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
 
-CREATE TABLE tr_bpkb (
-    agreement_number VARCHAR(100) PRIMARY KEY,
-    bpkb_no VARCHAR(100),
-    branch_id VARCHAR(10),
-    bpkb_date DATETIME,
-    faktur_no VARCHAR(100),
-    faktur_date DATETIME,
-    location_id VARCHAR(10),
-    FOREIGN KEY (location_id) REFERENCES ms_storage_location(location_id),
-    police_no VARCHAR(20),
-    bpkb_date_in DATETIME,
-    created_by VARCHAR(20),
-    created_on DATETIME,
-    last_updated_by VARCHAR(20),
-    last_updated_on DATETIME
-);
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tr_bpkb]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [dbo].[tr_bpkb](
+	[agreement_number] [varchar](100) NOT NULL,
+	[bpkb_no] [varchar](100) NULL,
+	[branch_id] [varchar](10) NULL,
+	[bpkb_date] [datetime] NULL,
+	[faktur_no] [varchar](100) NULL,
+	[faktur_date] [datetime] NULL,
+	[location_id] [varchar](10) NULL,
+	[police_no] [varchar](20) NULL,
+	[bpkb_date_in] [datetime] NULL,
+	[created_by] [varchar](20) NULL,
+	[created_on] [datetime] NULL,
+	[last_updated_by] [varchar](20) NULL,
+	[last_updated_on] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[agreement_number] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+END
+GO
 
+INSERT [dbo].[ms_storage_location] ([location_id], [location_name]) VALUES (N'1', N'Jakarta')
+INSERT [dbo].[ms_storage_location] ([location_id], [location_name]) VALUES (N'2', N'Bandung')
+
+SET IDENTITY_INSERT [dbo].[ms_user] ON 
+GO
+INSERT [dbo].[ms_user] ([user_id], [user_name], [password], [is_active]) VALUES (1, N'afifjunihar', N'admin123', 1)
+INSERT [dbo].[ms_user] ([user_id], [user_name], [password], [is_active]) VALUES (2, N'afif', N'admin', 1)
+
+SET IDENTITY_INSERT [dbo].[ms_user] OFF
+GO
+INSERT [dbo].[tr_bpkb] ([agreement_number], [bpkb_no], [branch_id], [bpkb_date], [faktur_no], [faktur_date], [location_id], [police_no], [bpkb_date_in], [created_by], [created_on], [last_updated_by], [last_updated_on]) VALUES (N'47', N'524', N'12', CAST(N'2007-03-25T00:00:00.000' AS DateTime), N'212', CAST(N'1978-04-02T00:00:00.000' AS DateTime), N'1', N'254', CAST(N'1976-11-17T00:00:00.000' AS DateTime), N'afifjunihar', CAST(N'2024-09-28T02:11:27.330' AS DateTime), N'afifjunihar', CAST(N'2024-09-28T02:11:27.330' AS DateTime))
+
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK__tr_bpkb__locatio__3B75D760]') AND parent_object_id = OBJECT_ID(N'[dbo].[tr_bpkb]'))
+ALTER TABLE [dbo].[tr_bpkb]  WITH CHECK ADD FOREIGN KEY([location_id])
+REFERENCES [dbo].[ms_storage_location] ([location_id])
+GO
